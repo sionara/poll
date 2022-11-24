7e267878a3578787cdb28220301e555849be1579
Sion <sionlee29@gmail.com>
2022-11-23 22:52:02 -0500

    created new views, model, and templates.

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
 # Create your views here.
-from django.http import HttpResponse
+from .models import Question, Choice
 
-def index(request):
-  return HttpResponse("Hello World!")
+class IndexView(generic.ListView):
+    template_name = 'myapp/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'myapp/detail.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'myapp/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('myapp:results', args=(question.id,)))
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'myapp/results.html'
